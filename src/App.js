c54ca6a40e8acb0953262bde8a8e40f5d8572d5d
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2022-11-07 09:41:42 -0600

    MVP tasks are finsihed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Form from './COMPONENTS/Form';
+import './App.css';
 
 
 function App() {
@@ -11,12 +12,34 @@ function App() {
     role: ''
   });
 
+
+  const onSubmit = () => {
+    setTeamMembers([values, ...teamMembers]);
+    setValues({name: '',
+    email: '',
+    role: ''})
+  }
+
+  const onChange = (name, value) => {
+    setValues({ ...values, [name]: value})
+  }
+
   return (
     <div className="App">
       <h1>Team Builder App</h1>
-      <Form values={values} />
-    </div>
-    
+      <Form 
+        values={values} 
+        change={onChange}
+        submit={onSubmit}
+        />
+        {teamMembers.map((teamMember, idx) => {
+          return (
+            <div key={idx}>
+              {teamMember.email}, {teamMember.name}, {teamMember.role}
+              </div>
+          )
+        })} 
+    </div>    
   );
 }
 
--- src/COMPONENTS/Form.js
+++ src/COMPONENTS/Form.js
@@ -2,17 +2,29 @@ import React from 'react';
 
 const Form = (props) => {
 
+  const handleChange = event => {
+    const { name, value } = event.target;
+    props.change(name, value);
+  }
+
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.submit();
+
+  }
+ 
   return (
     <div className='form'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Name:
           <input
           placeholder='Enter your name here.'
           type='text'
           name='name'
-          value=''
-          // onChange={handleChange}
+          value={props.values.name}
+          onChange={handleChange}
           />
         </label>
         <label>
@@ -21,13 +33,13 @@ const Form = (props) => {
           placeholder='Enter your email here.'
           type='email'
           name='email'
-          value=''
-          // onChange={handleChange}
+          value={props.values.email}
+          onChange={handleChange}
           />
         </label>
         <label>
           Role:
-          <select>
+          <select value={props.values.role} name='role' onChange={handleChange}>
             <option value=''>---Select an Option---</option>
             <option value='backendEngineer'>Backend Engineer</option>
             <option value='frontendEngineer'>Frontend Engineer</option>
